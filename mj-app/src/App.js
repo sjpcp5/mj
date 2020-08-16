659c2e6f699d80d5bb663e65e439e3771d7f0d73
Cutie on the couch <sjpcp5@gmail.com>
2020-08-16 17:35:57 -0500

    created pages index, blog-single, and resume
    added footer and header to app.js

--- mj-app/src/App.js
+++ mj-app/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import Header from "./components/header";
+import Footer from "./components/footer";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    );
   }
 }
 
